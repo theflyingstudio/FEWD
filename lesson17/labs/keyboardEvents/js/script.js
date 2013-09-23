e6c5341e1ebc0befbe1c1efc31ffafda3e3b48d5
Laura J. Perez <theflyingstudio@gmail.com>
2013-09-23 21:09:22 +0800

    final presentation

--- lesson17/labs/keyboardEvents/js/script.js
+++ lesson17/labs/keyboardEvents/js/script.js
@@ -1,11 +1,21 @@
 /**
  * Welcome to Keyboard Events Exercise
  * A couple of thing we want to do:
- * 
+ *
  * 1) First, let's display the most recent keyCode the user pressed. This will help you do the second
- *    exercise. 
+ *    exercise.
  *
  * 2) Now, let's change the behavior of the "noVowels" textbox so that no vowels can
- *    be typed in it. 
- *    Hint, remeber what event.preventDefault() does? 
+ *    be typed in it.
+ *    Hint, remeber what event.preventDefault() does?
  */
+
+/**$('#justpressed').keydown(function(event){
+	console.log(event.which)
+});
+
+*/
+
+$('#noVowels').keypress(function(event){
+	event.keycode)
+});
--- lesson17/labs/mouseEvents/js/script.js
+++ lesson17/labs/mouseEvents/js/script.js
@@ -1,11 +1,29 @@
 /**
  * Welcome to Mouse Events Exercise
  * A couple of thing we want to do:
- * 
- * 1) When a user clicks the mousebox, turn it red by adding the "clicked" css class 
- *    to it. When the user clicks it again, remove the "clicked" class and turn it blue 
+ *
+ * 1) When a user clicks the mousebox, turn it red by adding the "clicked" css class
+ *    to it. When the user clicks it again, remove the "clicked" class and turn it blue
  *    again.
  *
  * 2) Use the mouseMove event to show the current position of the mouse using the HTML snippet provided
  */
+$('#mouseBox').click(function(event){
+	$('#mouseBox').toggleClass('clicked');
+})
 
+$('#mouseBox').mousemove(function(event){
+	/**console.log(event.pageX)
+	console.log(event.pageY)
+
+	The following is code to get the mouse location to show on the coordinates in the
+		<p>
+        The mouse is at location (<span id="xLocation">0</span>, <span id="yLocation">0</span>)
+      	</p>*/
+
+$('#xLocation').text(event.pageX);
+$('#yLocation').text(event.pageY);
+});
+
+
+$("#mouseBox").on('mouseup mousedown',)
